c1febd0969b68134260314b2e8e3e0ff1049f2c9
asmsuechan <suenagaryoutaabc@gmail.com>
2016-12-27 00:12:55 +0900

    Adds abort on yml not exist

--- lib/yml2erd/cli.rb
+++ lib/yml2erd/cli.rb
@@ -1,15 +1,18 @@
 require "yml2erd/parser"
 require "yml2erd/diagram"
 require "thor"
+require "yaml"
 
 module Yml2erd
   class CLI < Thor
+    class YmlNotFoundError < StandardError;end
+
     desc "convert <path>", "Convert erd from yml"
     option :output, aliases: :o, banner: 'FILE_PATH', desc: 'default: output.png'
     option :projectname, aliases: :p, banner: 'PROJECT_NAME', desc: 'default: null'
     option :outputstyle, aliases: :s, banner: 'OUTPUT_STYLE', desc: 'svg or png, default: png'
     def convert(path)
-      schema_structure = Yml2erd::Parser.parse(path)
+      schema_structure = load_yml(path)
       opts = {
         output_path: options[:output_path],
         project_name: options[:projectname],
@@ -17,6 +20,14 @@ module Yml2erd
       }
       Yml2erd::Diagram.create(schema_structure, opts)
       puts 'Successfully converted!'
+    end
+
+    private
+
+    def load_yml(path)
+      abort "#{path} is not found, please check your .yml file path" unless File.exists?(path)
+      yml = YAML.load_file(path)
+      Yml2erd::Parser.parse(yml)
     end
   end
 end
--- lib/yml2erd/parser.rb
+++ lib/yml2erd/parser.rb
@@ -1,13 +1,11 @@
 require 'yml2erd/schema_structure'
 require 'yml2erd/schema_structure/validator'
-require 'yaml'
 
 module Yml2erd
   class Parser
     class << self
       # returns an object of Yml2erd::SchemaStructure
-      def parse(path)
-        yml = YAML.load_file(path)
+      def parse(yml)
         ss = Yml2erd::SchemaStructure.new(yml)
         Yml2erd::SchemaStructure::Validator.new(ss).validate
         ss
