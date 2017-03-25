93a95bc0f98eb48ea50e3df538fee7d7632e2f94
asmsuechan <suenagaryoutaabc@gmail.com>
2017-03-24 23:08:27 -0700

    :recycle: Refactor to use constant instead of string

--- lib/yml2erd/schema_structure.rb
+++ lib/yml2erd/schema_structure.rb
@@ -1,5 +1,11 @@
 module Yml2erd
   class SchemaStructure
+    SHARED_COLUMNS = 'shared_columns'.freeze
+    RELATIONS = 'relations'.freeze
+    BELONGS_TO = 'belongs_to'.freeze
+    COLUMNS = 'columns'.freeze
+    INDEX = 'index'.freeze
+    COMMENT = 'comment'.freeze
 
     attr_accessor :structure_yml, :table_names
 
@@ -11,19 +17,19 @@ module Yml2erd
     # returns an array which has table_names like bellow
     # => ["users", "user_auths", "posts", "companies"]
     def table_names
-      structure_yml.to_a.map { |s| s.first }.select{ |tn| tn != 'shared_columns' }
+      structure_yml.to_a.map { |s| s.first }.select{ |tn| tn != SHARED_COLUMNS }
     end
 
     # returns a hash like below
     # { belongs_to: ['user_auths', 'companies'], has_many: ['posts'] }
     def relation(table_name)
-      relations = structure_yml[table_name]['relations']
+      structure_yml[table_name][RELATIONS]
     end
 
     # returns an array like below
     # ['user_auths', 'companies']
     def belongs(table_name)
-      relation(table_name)['belongs_to']
+      relation(table_name)[BELONGS_TO]
     end
 
     def relations
@@ -39,25 +45,25 @@ module Yml2erd
     # returns an array like below
     # => [{"name"=>"string"}, {"email"=>"string"}]
     def columns(table_name)
-      structure_yml[table_name]['columns']
+      structure_yml[table_name][COLUMNS]
     end
 
     # returns an array like below
     # => ["email"]
     def index(table_name)
-      structure_yml[table_name]['index']
+      structure_yml[table_name][INDEX]
     end
 
     # returns a hash like below
     # => { "id"=>"integer", "created_at"=>"datetime", "updated_at"=>"datetime" }
     def shared_columns
-      structure_yml['shared_columns']
+      structure_yml[SHARED_COLUMNS]
     end
 
     # returns a string like below
     # => "This is users table"
     def comment(table_name)
-      structure_yml[table_name]['comment']
+      structure_yml[table_name][COMMENT]
     end
   end
 end
